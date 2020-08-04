9485aa795bca47460ddb40354babd5d74013a9e0
Felipe Souza <osouzafelipe@gmail.com>
2020-08-04 00:13:40 -0400

    atualizando porta

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,7 +13,6 @@ app.use(cors());
 app.use(express.json());
 app.use(require('./routes'));
 
-app.listen(3000,() =>{
+app.listen(process.env.PORT || 3000,() =>{
     console.log(':) Server Started on port 3000');
 })
-
